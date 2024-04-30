14832e7ab22682c22d2db298f6f0a28f805ed448
Gabriel Soares Maciel <gabriel.maciel@terceirizadoraizen.com>
2024-04-30 17:13:04 -0300

    feat:add to public route

--- subnet.tf
+++ subnet.tf
@@ -1,6 +1,7 @@
 resource "aws_subnet" "public_subnet" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.0.0/20"
+  availability_zone = "us-east-2b"
 
   tags = {
     type = "public"
@@ -10,6 +11,7 @@ resource "aws_subnet" "public_subnet" {
 resource "aws_subnet" "private_subnet" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.16.0/20"
+  availability_zone = "us-east-2b"
 
   tags = {
     type = "private"
