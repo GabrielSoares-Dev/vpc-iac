775ebb403a6a4637b4aa8c252d37c82f9096359a
Gabriel Soares Maciel <gabriel.maciel@terceirizadoraizen.com>
2024-05-03 16:14:45 -0300

    feat:add az subnets

--- subnet.tf
+++ subnet.tf
@@ -1,5 +1,5 @@
 resource "aws_subnet" "public_subnet" {
-  for_each = var.subnet_availability_zones
+  for_each = { for index, zone in var.subnet_availability_zones : index => zone }
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.0.0/20"
   availability_zone = each.value
@@ -10,7 +10,7 @@ resource "aws_subnet" "public_subnet" {
 }
 
 resource "aws_subnet" "private_subnet" {
-  for_each = var.subnet_availability_zones
+  for_each = { for index, zone in var.subnet_availability_zones : index => zone }
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.16.0/20"
   availability_zone = each.value
