37de43e68a925cf18a749a11789561248c31d6a5
Gabriel Soares Maciel <gabriel.maciel@terceirizadoraizen.com>
2024-05-03 15:53:44 -0300

    feat:add az subnets

--- route-table-association.tf
+++ route-table-association.tf
@@ -1,4 +1,5 @@
 resource "aws_route_table_association" "public_route_table_to_public_subnet" {
-  subnet_id      = aws_subnet.public_subnet.id
+  for_each = aws_subnet.public_subnet
+  subnet_id      = each.value.id
   route_table_id = aws_route_table.public_route_table.id
 }
