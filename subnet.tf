d56ae87386e47a23bb571571128bd563f51dc6e6
Gabriel Soares Maciel <gabriel.maciel@terceirizadoraizen.com>
2024-05-03 16:47:59 -0300

    feat: finalize subnets

--- subnet.tf
+++ subnet.tf
@@ -1,23 +1,23 @@
-# resource "aws_subnet" "public_subnet" {
-#   for_each = var.public_subnets
-#   vpc_id     = aws_vpc.main.id
-#   cidr_block = each.value.cidr_block
-#   availability_zone = each.value.availability_zone
+resource "aws_subnet" "public_subnet" {
+  for_each = var.public_subnets
+  vpc_id     = aws_vpc.main.id
+  cidr_block = each.value.cidr_block
+  availability_zone = each.value.availability_zone
 
-#   tags = {
-#     name = each.value.name
-#     type = "public"
-#   }
-# }
+  tags = {
+    name = each.value.name
+    type = "public"
+  }
+}
 
-# resource "aws_subnet" "private_subnet" {
-#   for_each = var.private_subnets
-#   vpc_id     = aws_vpc.main.id
-#   cidr_block = each.value.cidr_block
-#   availability_zone = each.value.availability_zone
+resource "aws_subnet" "private_subnet" {
+  for_each = var.private_subnets
+  vpc_id     = aws_vpc.main.id
+  cidr_block = each.value.cidr_block
+  availability_zone = each.value.availability_zone
 
-#   tags = {
-#     name = each.value.name
-#     type = "private"
-#   }
-# }
+  tags = {
+    name = each.value.name
+    type = "private"
+  }
+}
