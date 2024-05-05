44d9a49ab35eb817e6d57b7bb29d16e28f63e0a6
Gabriel Soares Maciel <gsm2015@Outlook.com.br>
2024-05-05 20:11:34 -0300

    fix: names and create private route table

--- route-table-association.tf
+++ route-table-association.tf
@@ -3,3 +3,9 @@ resource "aws_route_table_association" "public_route_table_to_public_subnet" {
   subnet_id      = each.value.id
   route_table_id = aws_route_table.public_route_table.id
 }
+
+resource "aws_route_table_association" "private_route_table_to_private_subnet" {
+  for_each       = aws_subnet.private_subnet
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.private_route_table.id
+}
