e6bbb79eaec1803a0c4bdbefb8f0ad5c7174464d
Gabriel Soares Maciel <gsm2015@Outlook.com.br>
2024-05-05 20:33:40 -0300

    feat: network acl

--- network-acl.tf
+++ network-acl.tf
@@ -2,7 +2,7 @@ resource "aws_network_acl" "public_network_acl" {
   vpc_id = aws_vpc.main.id
 
   ingress {
-    protocol   = "https"
+    protocol   = "HTTPS"
     rule_no    = 100
     action     = "allow"
     cidr_block = "0.0.0.0/0"
@@ -11,7 +11,7 @@ resource "aws_network_acl" "public_network_acl" {
   }
 
   egress {
-    protocol   = "https"
+    protocol   = "HTTPS"
     rule_no    = 200
     action     = "allow"
     cidr_block = "0.0.0.0/0"
