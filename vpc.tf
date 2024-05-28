df8e0d4d6cf123f556c76015a6b47c9d2ad8510b
Gabriel Soares Maciel <gsm2015@Outlook.com.br>
2024-05-28 20:19:48 -0300

    feat:change default route and acl

--- vpc.tf
+++ vpc.tf
@@ -2,6 +2,9 @@ resource "aws_vpc" "main" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true
 
+  main_route_table_id    = aws_route_table.public_route_table.id
+  default_network_acl_id = aws_network_acl.public_network_acl.id
+  default_route_table_id = aws_route_table.public_route_table.id
   tags = {
     Name = "Main VPC"
   }
