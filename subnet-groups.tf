6e465e7ee5ff67099e2a5d12e03afbdff562b608
Gabriel Soares Maciel <gabriel.maciel@terceirizadoraizen.com>
2024-05-28 16:57:09 -0300

    feat: subnets

--- subnet-groups.tf
+++ subnet-groups.tf
@@ -6,3 +6,12 @@ resource "aws_db_subnet_group" "private_subnet_group" {
     Name = "Private subnet group"
   }
 }
+
+resource "aws_db_subnet_group" "public_subnet_group" {
+  name       = "public_subnet_group"
+  subnet_ids = [for subnet in aws_subnet.public_subnet : subnet.id]
+
+  tags = {
+    Name = "Public subnet group"
+  }
+}
--- variables.tf
+++ variables.tf
@@ -6,18 +6,18 @@ variable "public_subnets" {
   }))
 
   default = {
-    first = {
-      name              = "first_public_subnet"
+    public_subnet_us-east_2a = {
+      name              = "public_subnet_us-east_2a"
       cidr_block        = "10.0.0.0/20"
       availability_zone = "us-east-2a"
     },
-    second = {
-      name              = "second_public_subnet"
+    public_subnet_us-east_2b = {
+      name              = "public_subnet_us-east_2b"
       cidr_block        = "10.0.16.0/20"
       availability_zone = "us-east-2b"
     },
-    third = {
-      name              = "third_public_subnet"
+    public_subnet_us-east_2c = {
+      name              = "public_subnet_us-east_2c"
       cidr_block        = "10.0.32.0/20"
       availability_zone = "us-east-2c"
     }
@@ -32,18 +32,18 @@ variable "private_subnets" {
   }))
 
   default = {
-    first = {
-      name              = "first_private_subnet"
+    private_subnet_us-east_2a = {
+      name              = "private_subnet_us-east_2a"
       cidr_block        = "10.0.48.0/20"
       availability_zone = "us-east-2a"
     },
-    second = {
-      name              = "second_private_subnet"
+    private_subnet_us-east_2b = {
+      name              = "private_subnet_us-east_2b"
       cidr_block        = "10.0.64.0/20"
       availability_zone = "us-east-2b"
     },
-    third = {
-      name              = "third_private_subnet"
+    private_subnet_us-east_2c = {
+      name              = "private_subnet_us-east_2c"
       cidr_block        = "10.0.80.0/20"
       availability_zone = "us-east-2c"
     }
