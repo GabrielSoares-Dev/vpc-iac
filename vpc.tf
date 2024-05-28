52d23bdca398466a26ff5649dc49b5737d7754dd
Gabriel Soares Maciel <gsm2015@Outlook.com.br>
2024-05-28 20:36:29 -0300

    feat:remove default vpc

--- vpc.tf
+++ vpc.tf
@@ -1,14 +1,8 @@
 resource "aws_vpc" "main" {
-  cidr_block           = "172.31.0.0/16"
+  cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true
 
   tags = {
     Name = "Main VPC"
-  }
-}
-
-resource "aws_default_vpc" "default" {
-  tags = {
-    Name = "Default VPC"
   }
 }
