b9f5cff851016254afacddc3b03f512826902208
Gabriel Soares Maciel <gsm2015@Outlook.com.br>
2024-05-29 22:34:11 -0300

    feat: mysql rules

---
+++ network-acl-rules.tf
@@ -0,0 +1,10 @@
+resource "aws_network_acl_rule" "allow_mysql_inbound" {
+  network_acl_id = aws_network_acl.private_network_acl.id
+  rule_number    = 100
+  protocol       = "tcp"
+  rule_action    = "allow"
+  egress         = false
+  cidr_block     = "0.0.0.0/0"
+  from_port      = 3306
+  to_port        = 3306
+}
--- network-acl.tf
+++ network-acl.tf
@@ -1,20 +1,6 @@
 resource "aws_network_acl" "public_network_acl" {
   vpc_id = aws_vpc.main.id
 
-  ingress {
-    protocol   = "-1" 
-    rule_no    = 100
-    action     = "allow"
-    cidr_block = "0.0.0.0/0"
-  }
-
-  egress {
-   protocol   = "-1" 
-    rule_no    = 100
-    action     = "allow"
-    cidr_block = "0.0.0.0/0"
-  }
-
   tags = {
     Name = "public_network_acl"
   }
