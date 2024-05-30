e59a28ad25f6896eb2eb9f87042f71caf33bb16e
Gabriel Soares Maciel <gsm2015@Outlook.com.br>
2024-05-30 18:05:20 -0300

    feat: add rules

--- network-acl-rules.tf
+++ network-acl-rules.tf
@@ -8,3 +8,27 @@ resource "aws_network_acl_rule" "allow_mysql_inbound" {
   from_port      = 3306
   to_port        = 3306
 }
+
+resource "aws_network_acl_rule" "allow_mysql_outbound" {
+  network_acl_id = aws_network_acl.public_network_acl.id
+  rule_number    = 100
+  protocol       = "tcp"
+  rule_action    = "allow"
+  egress         = true
+  cidr_block     = "0.0.0.0/0"
+  from_port      = 3306
+  to_port        = 3306
+}
+
+
+
+resource "aws_network_acl_rule" "allow_all_outbound" {
+  network_acl_id = aws_network_acl.public_network_acl.id
+  rule_number    = 200
+  protocol       = "-1"
+  rule_action    = "allow"
+  egress         = true
+  cidr_block     = "0.0.0.0/0"
+  from_port      = 0
+  to_port        = 0
+}
