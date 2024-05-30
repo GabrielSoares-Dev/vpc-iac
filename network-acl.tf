1b1871daa00f56479383048284a3694734184b2e
Gabriel Soares Maciel <gsm2015@Outlook.com.br>
2024-05-29 22:30:38 -0300

    feat: remove network acl

--- network-acl.tf
+++ network-acl.tf
@@ -1,6 +1,20 @@
 resource "aws_network_acl" "public_network_acl" {
   vpc_id = aws_vpc.main.id
 
+  ingress {
+    protocol   = "-1" 
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+  }
+
+  egress {
+   protocol   = "-1" 
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+  }
+
   tags = {
     Name = "public_network_acl"
   }
